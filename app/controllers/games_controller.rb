96dd650635d190fe70cb1637488fabc70c20af53
Ege Hidiroglu <egehdrgl@gmail.com>
2022-08-08 15:04:00 -0400

    Finished implementing the game logic. Started adding score and trial mechanics.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     alphabet = ("A".."Z").to_a
@@ -9,25 +12,51 @@ class GamesController < ApplicationController
     end
   end
 
+  def array_to_hash(array)
+    hash = {}
+    array.each do |letter|
+      if hash[letter.downcase].nil?
+        hash[letter.downcase] = 1
+      else
+        hash[letter.downcase] += 1
+      end
+    end
+    hash
+  end
+
+  def grid(letter_hash, answer_hash)
+    bool = true
+    answer_hash.each do |key|
+      bool = false if letter_hash[key[0]].nil? || answer_hash[key[0]] > letter_hash[key[0]]
+    end
+    bool
+  end
+
+  def result(grid, english, answer, letters)
+    if !grid
+      result = "Sorry but #{answer} can't be built out of #{letters}"
+      score = 0
+    elsif grid && !english
+      result = "Sorry but #{answer} does not seem to be a valid English word..."
+      score = 0
+    elsif grid && english
+      result = "Congratulations! #{answer} is a valid English word!"
+      score = answer.length
+    end
+    [result, score]
+  end
+
   def score
-    @answer = params[:answer]
-    @letters = params[:letters]
-    @letters_hash = {}
-    @answer_hash = {}
-    @letters.split.each do |letter|
-      if @letters_hash[letter.downcase].nil?
-        @letters_hash[letter.downcase] = 1
-      else
-        @letters_hash[letter.downcase] += 1
-      end
-    end
-    @answer.downcase.chars do |letter|
-      if @answer_hash[letter.downcase].nil?
-        @answer_hash[letter.downcase] = 1
-      else
-        @answer_hash[letter.downcase] += 1
-      end
-    end
-    raise
+    @answer = params[:answer].upcase
+    @letters = params[:letters].upcase
+    @letters_hash = array_to_hash(@letters.split)
+    @answer_hash = array_to_hash(@answer.downcase.chars)
+    @grid = grid(@letters_hash, @answer_hash)
+    url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
+    response_serialized = URI.open(url).read
+    @english = JSON.parse(response_serialized)['found']
+    @result = result(@grid, @english, @answer, @letters)[0]
+    session[:score] += result(@grid, @english, @answer, @letters)[1]
+    session[:try] += 1
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get '/new', to: 'games#new'
 
   post '/score', to: 'games#score'
+
+  get '/score', to: 'games#score'
 end
